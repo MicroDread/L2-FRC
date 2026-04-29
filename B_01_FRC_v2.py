1961c10aa5ca26821ed15dcb1a11a80c43e0acd7
MicroDread <mutangak3654@masseyhigh.school.nz>
2026-04-29 12:26:50 +1200

    Add files via upload

--- B_01_FRC_v2.py
+++ B_01_FRC_v2.py
@@ -1,6 +1,7 @@
 import pandas
 from tabulate import tabulate
 from datetime import date
+import math
 
 
 # Functions go here
@@ -116,7 +117,7 @@ def get_expenses(exp_type, how_many=1):
     }
 
     # defaults for fixed expenses
-    amount = how_many   # how_many defaults to 1
+    amount = how_many  # how_many defaults to 1
     how_much_question = "How much? $"
 
     # loop to get expenses
@@ -150,6 +151,7 @@ def get_expenses(exp_type, how_many=1):
 
         # Get price for item (question customised depending on expense type).
         price_for_one = num_check(how_much_question, "float")
+        print()
 
         all_items.append(item_name)
         all_amounts.append(amount)
@@ -184,6 +186,118 @@ def get_expenses(exp_type, how_many=1):
 def currency(x):
     """Formats numbers as currency ($#.##)"""
     return "${:.2f}".format(x)
+
+
+def profit_goal(total_costs):
+    """Calculates profit goal  work out profit goal and total sales required"""
+    # Initialise variables and error message
+    error = "Please enter a valid profit goal\n"
+
+    valid = False
+    while not valid:
+
+        # ask for profit goal...
+        response = input("What is your profit goal (eg $500 or 50%): ")
+
+        # check if first character is $...
+        if response[0] == "$":
+            profit_type = "$"
+            # Get amount (everything after the $)
+            amount = response[1:]
+
+        # check if last character is %
+        elif response[-1] == "%":
+            profit_type = "%"
+            # Get amount (everything before the %)
+            amount = response[:-1]
+
+        else:
+            # set response to amount for now
+            profit_type = "unknown"
+            amount = response
+
+        try:
+            # Check amount is a number more than zero...
+            amount = float(amount)
+            if amount <= 0:
+                print(error)
+                continue
+
+        except ValueError:
+            print(error)
+            continue
+
+        if profit_type == "unknown" and amount >= 100:
+            dollar_type = yes_no_check(f"Do you mean ${amount:.2f}.  ie {amount:.2f} dollars? , y / n: ")
+
+            # Set profit type based on user answer above
+            if dollar_type == "yes":
+                profit_type = "$"
+            else:
+                profit_type = "%"
+
+        elif profit_type == "unknown" and amount < 100:
+            percent_type = yes_no_check(f"Do you mean {amount}%? , y / n: ")
+            if percent_type == "yes":
+                profit_type = "%"
+            else:
+                profit_type = "$"
+
+        # return profit goal to main routine
+        if profit_type == "$":
+            return amount
+        else:
+            goal = (amount / 100) * total_costs
+            return goal
+
+
+def round_up(amount, round_val):
+    """Rounds amount to desired hole number"""
+    return int(math.ceil(amount / round_val)) * round_val
+
+
+def clean_filename(raw_filename):
+    """Check filename has not illegal characters and is not too long"""
+
+    # assume filename is OK
+    valid_filename = True
+    error = ""
+
+    while True:
+
+        # replace spaces with underscores
+        raw_filename = raw_filename.replace(" ", "_")
+
+        # check for valid length
+        if len(raw_filename) >= 20:
+            valid_filename = False
+            error = ("Oops - your product name / filename is too long.  \n"
+                     "Please provide an alternate filename (<= 19 characters) \n"
+                     "or press <enter> to default to FRC_yyyy_mm_ddd")
+
+        # iterate through filename and check for invalid characters
+        for letter in raw_filename:
+            if letter.isalnum() is False and letter != "_":
+                valid_filename = False
+                error = ("I can't use the product name / proposed filename \n"
+                         "as it has illegal characters.  Please \n"
+                         "enter an alternate name for the first part \n"
+                         "of the file or press <enter> to default to FRC_yyyy_mm_dd")
+                break
+
+        if valid_filename is False:
+            print(error)
+            raw_filename = input("\nPlease enter an alternate name for the start of the file: ")
+
+            # reset valid_filename so that it new name can be checked.
+            valid_filename = True
+
+            # put in default filename if users press <enter>
+            if raw_filename == "":
+                raw_filename = "FRC"
+
+        else:
+            return raw_filename
 
 
 # Main routine goes here
@@ -235,8 +349,15 @@ if has_fixed == "yes":
 total_expenses = variable_subtotal + fixed_subtotal
 total_expenses_string = f"Total Expenses: ${total_expenses:.2f}"
 
-
 # Get profit Goal here.
+target = profit_goal(total_expenses)
+sales_target = total_expenses + target
+
+# Calculate minimum selling price and round
+# it to nearest desired dollar amount
+selling_price = (total_expenses + target) / quantity_made
+round_to = num_check("Round To: ", 'integer')
+suggested_price = round_up(selling_price, round_to)
 
 # strings / output area
 
@@ -265,12 +386,23 @@ else:
     fixed_heading_string = make_statement("You have no Fixed Expenses", "-")
     fixed_subtotal_string = "Fixed Expenses Subtotal: $0.00"
 
+selling_price_heading = make_statement("Selling Price Calculations", "-")
+profit_goal_string = f"Profit Goal: ${target:.2f}"
+sales_target_string = f"\nTotal Sales Needed: ${sales_target:.2f}"
+
+minimum_price_string = f"Minimum Selling Price: ${selling_price:.2f}"
+suggested_price_string = make_statement(f"Suggested Selling Price: "
+                                        f"${suggested_price:.2f}", "*")
+
 # List of strings to be outputted / written to file
 to_write = [main_heading_string, quantity_string,
             "\n", variable_heading_string, variable_panda_string,
             variable_subtotal_string,
             "\n", fixed_heading_string, fixed_panda_string,
-            fixed_subtotal_string, total_expenses_string]
+            fixed_subtotal_string, "\n",
+            selling_price_heading, total_expenses_string,
+            profit_goal_string, sales_target_string,
+            minimum_price_string, "\n", suggested_price_string]
 
 # Print area
 print()
@@ -278,7 +410,12 @@ for item in to_write:
     print(item)
 
 # create file to hold data (add .txt extension)
-file_name = f"{product_name}_{year}_{month}_{day}"
+
+# check product name is suitable for a filename
+# and ask for an alternate file name if necessary
+clean_product_name = clean_filename(product_name)
+
+file_name = f"{clean_product_name}_{year}_{month}_{day}"
 write_to = "{}.txt".format(file_name)
 
 text_file = open(write_to, "w+")
